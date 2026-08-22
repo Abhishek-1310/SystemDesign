0a225189b135f5d86885d94262a5f5ca3fa70cfa
Abhishek Kumar yadav <76660763+Abhishek-1310@users.noreply.github.com>
2026-08-22 11:31:00 +0530

    Update Rideservice.js

--- LLD/Interview_Question_Of_LLD/RideSharingApp/rideService/Rideservice.js
+++ LLD/Interview_Question_Of_LLD/RideSharingApp/rideService/Rideservice.js
@@ -48,3 +48,56 @@ class RideService {
 }
 
 module.exports = RideService;
+
+
+const Ride = require('./ride')
+
+class RideService {
+    constructor(rideMatchingService) {
+        this.rideMatchingService = rideMatchingService;
+    }
+
+    requestRide(
+        rideId,
+        passenger,
+        distance,
+        fareStrategy
+    ) {
+        const driver = this.rideMatchingService.findNearestDriver(
+            passenger.location
+        );
+
+        if (!driver) {
+            throw new Error("No drivers available.");
+        }
+
+        driver.available = false;
+
+        const ride = new Ride(
+            rideId,
+            passenger,
+            driver,
+            distance,
+            fareStrategy
+        );
+
+        // FIX: This passes the ride details directly to the driver instance
+        driver.currentRide = ride; 
+
+        ride.attach(passenger);
+        ride.attach(driver);
+
+        return ride;
+    }
+
+    startRide(ride) {
+        ride.startRide();
+    }
+
+    completeRide(ride) {
+        ride.completeRide();
+        return ride.calculateFare();
+    }
+}
+
+module.exports = RideService;
